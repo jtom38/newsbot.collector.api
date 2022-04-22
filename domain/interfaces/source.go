1d4d27e7851e0b3ba0ea6e9a458c883c2a0ef26b
James Tombleson <luther38@gmail.com>
2022-04-21 17:14:23 -0700

    working on getting the standard interface for sources based on the work for ffxiv

--- domain/interfaces/source.go
+++ domain/interfaces/source.go
@@ -1,10 +1,17 @@
 package interfaces
 
 import (
+	"github.com/go-rod/rod"
 	"github.com/mmcdole/gofeed"
 )
 
 type Sources interface {
 	CheckSource() error
 	PullFeed() (*gofeed.Feed, error)
+
+	GetBrowser() *rod.Browser
+	GetPage(url string) *rod.Page
+
+	ExtractThumbnail(page *rod.Page) (string, error)
+	ExtractPubDate(page *rod.Page) (string, error)
 }
