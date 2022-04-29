de03f0022a1cc558713ffa75bb40dd33c9628db8
James Tombleson <luther38@gmail.com>
2022-04-28 22:27:27 -0700

    moved config to its own package and added basic tests

---
+++ services/config/config_test.go
@@ -0,0 +1,20 @@
+package config_test
+
+import (
+	"testing"
+	"os"
+
+	"github.com/jtom38/newsbot/collector/services/config"
+)
+
+func TestNewClient(t *testing.T) {
+	config.New()
+}
+
+func TestGetConfigExpectNull(t *testing.T) {
+	cc := config.New()
+	os.Setenv(config.REDDIT_PULL_HOT, "")
+	res := cc.GetConfig(config.REDDIT_PULL_HOT)
+	if res != "" { panic("expected blank")}
+
+}
