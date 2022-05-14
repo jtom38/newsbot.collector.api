59f74a19398bf7bc8063ddf98c54dc0e7a53d296
James Tombleson <luther38@gmail.com>
2022-05-14 09:40:13 -0700

    added a new err for invalid author images

--- services/common.go
+++ services/common.go
@@ -4,11 +4,14 @@ import "errors"
 
 var (
 	ErrMissingAuthorName  = errors.New("unable to find the post author name")
-	ErrMessingAuthorImage = errors.New("unable to find the post author image")
+	ErrMissingAuthorImage = errors.New("unable to find the post author image")
 	ErrMissingThumbnail   = errors.New("unable to find the post thumbnail url")
 	ErrMissingPublishDate = errors.New("unable to find the post publish date")
 	ErrMissingTags        = errors.New("unable to find the post tags")
 	ErrMissingDescription = errors.New("unable to find the post description")
+	ErrMissingUrl         = errors.New("unable to find the post url")
+
+	ErrInvalidAuthorImage = errors.New("expected value looks to be wrong, something is missing")
 )
 
 const DATETIME_FORMAT string = "1/2/2006 3:4 PM"
