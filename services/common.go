ede6feb4a0932edbcb528aff4999d6a7f6b829b8
James Tombleson <luther38@gmail.com>
2022-05-14 08:26:03 -0700

    added common errors

--- services/common.go
+++ services/common.go
@@ -3,11 +3,12 @@ package services
 import "errors"
 
 var (
-	ErrMissingAuthorName  = errors.New("unable to find the author name")
-	ErrMessingAuthorImage = errors.New("unable to find the author image")
-	ErrMissingThumbnail   = errors.New("unable to find the thumbnail url")
-	ErrMissingPublishDate = errors.New("unable to find the publish date")
-	ErrMissingTags        = errors.New("unable to find the tags")
+	ErrMissingAuthorName  = errors.New("unable to find the post author name")
+	ErrMessingAuthorImage = errors.New("unable to find the post author image")
+	ErrMissingThumbnail   = errors.New("unable to find the post thumbnail url")
+	ErrMissingPublishDate = errors.New("unable to find the post publish date")
+	ErrMissingTags        = errors.New("unable to find the post tags")
+	ErrMissingDescription = errors.New("unable to find the post description")
 )
 
 const DATETIME_FORMAT string = "1/2/2006 3:4 PM"
