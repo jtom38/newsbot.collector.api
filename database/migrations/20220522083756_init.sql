b3f06463dafbf3740b8132f0aecbbc393f4963d8
James Tombleson <luther38@gmail.com>
2022-06-08 20:34:17 -0700

    updated the seed migration

--- database/migrations/20220522083756_init.sql
+++ database/migrations/20220522083756_init.sql
@@ -3,16 +3,16 @@
 SELECT 'up SQL query';
 CREATE TABLE Articles (
 	ID 			uuid PRIMARY KEY,
-	SourceId 	uuid not null,
-	Tags 		TEXT,
-	Title 		TEXT,
-	Url 		TEXT,
-	PubDate 	timestamp,
+	SourceId 	uuid NOT null,
+	Tags 		TEXT NOT NULL,
+	Title 		TEXT NOT NULL,
+	Url 		TEXT NOT NULL,
+	PubDate 	timestamp NOT NULL,
 	Video 		TEXT,
-	VideoHeight int,
-	VideoWidth 	int,
-	Thumbnail 	TEXT,
-	Description TEXT,
+	VideoHeight int NOT NULL,
+	VideoWidth 	int NOT NULL,
+	Thumbnail 	TEXT NOT NULL,
+	Description TEXT NOT NULL,
 	AuthorName 	TEXT,
 	AuthorImage TEXT
 );
@@ -24,38 +24,39 @@ CREATE Table DiscordQueue (
 
 CREATE Table DiscordWebHooks (
 	ID 		uuid PRIMARY KEY,
-	Name 	TEXT,
+	Name 	TEXT NOT NULL, -- Defines webhook purpose
 	Key 	TEXT,
-	Url 	TEXT,
-	Server 	TEXT,
-	Channel TEXT,
-	Enabled BOOLEAN
+	Url 	TEXT NOT NULL, -- Webhook Url
+	Server 	TEXT NOT NULL, -- Defines the server its bound it. Used for refrence
+	Channel TEXT NOT NULL, -- Defines the channel its bound to.  Used for refrence
+	Enabled BOOLEAN NOT NULL
 );
 
 CREATE Table Icons (
 	ID 			uuid PRIMARY Key,
-	FileName 	TEXT,
-	Site 		TEXT
+	FileName 	TEXT NOT NULL,
+	Site 		TEXT NOT NULL
 );
 
 Create Table Settings (
 	ID 		uuid PRIMARY Key,
-	Key 	TEXT,
-	Value 	TEXT,
-	Options TEXT
+	Key 	TEXT NOT NULL, -- How you search for a entry
+	Value 	TEXT NOT NULL, -- The value for one
+	Options TEXT -- any notes about the entry
 );
 
 Create Table Sources (
 	ID 		uuid PRIMARY Key,
-	Site 	TEXT,
-	Name 	TEXT,
-	Source 	TEXT,
-	Type 	TEXT,
+	Site 	TEXT NOT NULL, -- Vanity name
+	Name 	TEXT NOT NULL, -- Defines the name of the source. IE: dadjokes
+	Source 	TEXT NOT NULL, -- Defines the service that will use this reocrd. IE reddit or youtube
+	Type 	TEXT NOT NULL, -- Defines what kind of feed this is.  feed, user, tag
 	Value 	TEXT,
-	Enabled BOOLEAN,
-	Url 	TEXT,
-	Tags 	TEXT
+	Enabled BOOLEAN NOT NULL,
+	Url 	TEXT NOT NULL,
+	Tags 	TEXT NOT NULL
 );
+
 
 
 -- +goose StatementEnd
