9b4a4a0ae484d7f5361f9b69e8e8b7914aab6a53
James Tombleson <luther38@gmail.com>
2022-07-14 09:58:02 -0700

    updated swagger to reflect the dynamic uri

--- docs/docs.go
+++ docs/docs.go
@@ -518,7 +518,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "0.1",
-	Host:             "localhost:8081",
+	Host:             "",
 	BasePath:         "/api",
 	Schemes:          []string{},
 	Title:            "NewsBot collector",
