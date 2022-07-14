f01a8ff592905cf98fe5b9f83dcdbfafe32c3dcd
James Tombleson <luther38@gmail.com>
2022-07-13 21:31:53 -0700

    Features/swagger update (#14)
    * ArticlesByTag added
    
    * format cleanup

--- main.go
+++ main.go
@@ -10,11 +10,10 @@ import (
 	"github.com/jtom38/newsbot/collector/services/cron"
 )
 
-
-
 // @title     NewsBot collector
 // @version   0.1
 // @BasePath  /api
+
 func main() {
 	ctx := context.Background()
 	c := cron.New(ctx)
@@ -26,5 +25,7 @@ func main() {
 	fmt.Println("API: http://localhost:8081/api")
 	fmt.Println("Swagger: http://localhost:8081/swagger/index.html")
 	err := http.ListenAndServe(":8081", server.Router)
-	if err != nil { panic(err) }
+	if err != nil { 
+		panic(err)
+	}
 }
